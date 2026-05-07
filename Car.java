4fb1c70ba5356cc0ca9ea078ede0d817f922e66b
Chris <you@pace.edu>
2026-05-07 11:18:52 -0400

    car.java & maintenanceRecords

--- Car.java
+++ Car.java
@@ -1,3 +1,55 @@
 public class Car {
-    
+    private String make;
+    private String model;
+    private int year;
+    private int mileage;
+    private String VIN;
+
+    public Car(String make, String model, int year, int mileage, String VIN) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.VIN = VIN;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getVIN() {
+        return VIN;
+    }
+
+    public void setVIN(String VIN) {
+        this.VIN = VIN;
+    }
 }
--- MaintenanceRecord.java
+++ MaintenanceRecord.java
@@ -1,3 +1,52 @@
 public class MaintenanceRecord {
-    
+    private String serviceName;
+    private int mileage;
+    private String date;
+    private String notes;
+
+    public MaintenanceRecord(String serviceName, int mileage, String date, String notes) {
+        this.serviceName = serviceName;
+        this.mileage = mileage;
+        this.date = date;
+        this.notes = notes;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String toString() {
+        return "Service Name: " + serviceName +
+               "\nMileage: " + mileage +
+               "\nDate: " + date +
+               "\nNotes: " + notes;
+    }
 }
