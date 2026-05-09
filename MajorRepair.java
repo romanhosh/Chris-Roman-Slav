c100497d2d8a5fd04f542f37b6f4e96cb9a78fbd
Roman <rh95255p@pace.edu>
2026-05-09 09:04:15 -0400

    test test 123

--- MajorRepair.java
+++ MajorRepair.java
@@ -1,3 +1,21 @@
-public class MajorRepair {
-    
+public class MajorRepair extends MaintenanceRecord {
+
+    private double partsCost;
+
+    public MajorRepair(String serviceName, int mileage, String date, String notes, double partsCost) {
+        super(serviceName, mileage, date, notes);
+        this.partsCost = partsCost;
+    }
+
+    public double getPartsCost() {
+        return partsCost;
+    }
+
+    public void setPartsCost(double partsCost) {
+        this.partsCost = partsCost;
+    }
+
+    public String toString() {
+        return super.toString() + "\nParts Cost: $" + partsCost + "\nType: Major Repair";
+    }
 }
--- RoutineService.java
+++ RoutineService.java
@@ -1,3 +1,21 @@
-public class RoutineService {
-    
+public class RoutineService extends MaintenanceRecord {
+
+    private int serviceInterval;
+
+    public RoutineService(String serviceName, int mileage, String date, String notes, int serviceInterval) {
+        super(serviceName, mileage, date, notes);
+        this.serviceInterval = serviceInterval;
+    }
+
+    public int getServiceInterval() {
+        return serviceInterval;
+    }
+
+    public void setServiceInterval(int serviceInterval) {
+        this.serviceInterval = serviceInterval;
+    }
+
+    public String toString() {
+        return super.toString() + "\nService Interval: every " + serviceInterval + " miles" + "\nType: Routine Service";
+    }
 }
